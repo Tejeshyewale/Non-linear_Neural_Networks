2e71bfdb789aa0a318b2d9f6bcf06ec7df0d48fd
Tejesh yewale <tejeshyewale917@gmail.com>
2026-03-10 17:40:47 +0530

    Create run.py

---
+++ run.py
@@ -0,0 +1,10 @@
+import cv2
+import numpy as np
+
+print("Age and Gender Prediction Project")
+
+print("Setup completed successfully.")
+print("Please open the Jupyter Notebook to run the model.")
+
+print("Run the following command:")
+print("jupyter notebook age_gender_revised.ipynb")
